39c766271318c829d780a9f6229906b84528f2ca
Benny Boy <benjaminwoodard93@gmail.com>
2023-10-04 14:24:21 -0400

    sentence functions generate random index and interpolate

--- index.js
+++ index.js
@@ -2,34 +2,34 @@
 // intro phrase array of strings
 
  
-    const intros = ['Before 8am', 'This morning', 'This week', 
-      'After Dinner', 'After work'];
+    const intros = ['Before 8am', 'This morning', 'Before 5pm', 
+                  'After Dinner', 'After work'];
 
     const midSentence = ['run 1 mile', 'drink 2 glasses of water',
                   'do 20 pushups','stretch for 10 minutes', 
-                  'meditate for 10 minutes' ];
+                  'walk for 30 minutes'];
     
+    const endSentence = ['turn your phone off for 1 hour.', 'write down 10 things you are grateful for.',
+                    'call a loved one or famly member.', 'read a book for 30 minutes.', 
+                    'spend an hour doing something you love.'];
 
 //generate random number to be index value
 function genRandNum(num){
     return Math.floor(Math.random() * num)};
 
-const genIntro = () =>{
-    let intro = [];
-    let introIdx = genRandNum(intros.length);
-    intro.push(intros[introIdx]);
-    return intro;
-}
-
-const genMid = () =>{
-    let midSen = [];
-    let midIdx = genRandNum(midSentence.length);
-    midSen.push(midSentence[midIdx]);
-    return midSen;
-}    
+//nest functions with random idx to pull a statement above arrays. 
+// interpolate together. 
 
 const getAdvice = () =>{
     let advice = '';
+
+    //generate random index for intro, midsentence, endsentence
+    const genIntro = () =>{
+        let intro = [];
+        let introIdx = genRandNum(intros.length);
+        intro.push(intros[introIdx]);
+        return intro;
+    }
 
     const genMid = () =>{
         let midSen = [];
@@ -37,6 +37,17 @@ const getAdvice = () =>{
         midSen.push(midSentence[midIdx]);
         return midSen;
     }    
+
+    const genEnd = () =>{
+        let endSen = [];
+        let endIdx = genRandNum(endSentence.length);
+        endSen.push(endSentence[endIdx]);
+        return endSen;
+    }    
+   
+    //interpolate and return values of sentence functions
     
-}
+    advice = `${genIntro()} ${genMid()} and ${genEnd()}`;
+    return advice;
+};
 console.log(getAdvice())
